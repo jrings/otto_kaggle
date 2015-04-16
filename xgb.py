69b63266a717a6657c47caed0bc98ac27d1ce675
Joerg Rings <mail@rings.de>
2015-04-16 01:40:59 +0000

    ENH: Full simple XGBoost submission

--- xgb.py
+++ xgb.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.cross_validation import cross_val_score
-import xgboost
+import xgboost as xgb
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+train = pd.read_csv("../train.csv")
+test = pd.read_csv("../test.csv")
 
 X = pd.concat((train[[col for col in train.columns if col.startswith("feat")]], test))
 count_lk = {col: X.groupby(col).aggregate({"id": "count"}).to_dict()["id"] for col in X.columns}
@@ -23,8 +23,8 @@ for col in train.columns:
 print(train.head())
 
 
-param = {'max_depth': 3, 'eta': 0.05, 'silent':1, 'objective':'binary:logistic',
-         'nthread': 4, 'eval_metric': 'logloss', 'seed': 1979 }
+param = {'max_depth': 3, 'eta': 0.05, 'silent':1, 'objective':'multi:softprob',
+         'nthread': 4, 'eval_metric': 'mlogloss', 'seed': 1979, 'num_class': 9 }
 
 num_round = 150
 
@@ -33,6 +33,11 @@ dtest = xgb.DMatrix(np.array(test))
 bst = xgb.train(param, dtrain, num_round)
 preds = bst.predict(dtest)
 
-
-print(preds)
+with open("submit_xgb_one.csv", "w") as _o:
+    print("id,Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7,Class_8,Class_9", file=_o)
+    for i in range(preds.shape[0]):
+        id_ = i + 1
+        row = ['{}'.format(id_)] + ["{}".format(x) for x in preds[i, :]]
+        print(",".join(row), file=_o)
+        
 
